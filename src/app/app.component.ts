7afe1555d07fbd6708f1f6bde23d6d8f231573db
MuriloBSD <murilo@lab804.com.br>
2018-07-26 19:00:27 -0300

    adicionado o carrinho

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,8 +9,7 @@ export class AppComponent {
   menuState: string = "show";
 
   quadros: Array<string> = ["modulra 1", "modulra 1", "modulra 1", 
-  "modulra 1", "modulra 1", "modulra 1", "modulra 1", "modulra 1", "modulra 1", 
-  "modulra 1", "modulra 1", "modulra 1", "modulra 1", "modulra 1", ];
+  "modulra 1", "modulra 1", "modulra 1", "modulra 1", "modulra 1"];
 
   toggleMenu() {
     this.menuState = this.menuState === 'show' ? 'hidden' : 'show';
