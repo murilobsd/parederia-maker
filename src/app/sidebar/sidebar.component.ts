fd039f101bc7bf56aa1331c69cfec99f71607ac7
MuriloBSD <murilo@lab804.com.br>
2018-08-02 14:48:33 -0300

    obtendo quadros apartir do service

--- src/app/sidebar/sidebar.component.ts
+++ src/app/sidebar/sidebar.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 import { Quadro } from '../quadro/quadro.model';
+import {QuadrosService} from '../shared';
 
 @Component({
   selector: 'mk-sidebar',
@@ -9,17 +10,24 @@ import { Quadro } from '../quadro/quadro.model';
 export class SidebarComponent implements OnInit {
 
   @Input() state = 'show';
-  @Input() quadros: Array<Quadro> = [];
   @Output() clicadoQuadro = new EventEmitter();
+  quadros: Array<Quadro> = [];
 
-  constructor() { }
+  constructor(private quadroService: QuadrosService) { }
 
   ngOnInit() {
+    this.obterTodosQuadros();
   }
 
   adicionarQuadro(quadro: Quadro) {
     console.log('Clicado quadro: ', quadro.name);
     this.clicadoQuadro.emit(quadro);
+  }
+
+  obterTodosQuadros() {
+    this.quadroService.getAll().subscribe(
+      (data: any) => { this.quadros = data; },
+      error => console.log(error));
   }
 
 }
