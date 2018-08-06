dd9812fb4c9d6407230d74ee02943bc5670f88c0
MuriloBSD <murilo@lab804.com.br>
2018-08-06 17:21:26 -0300

    criado service de cores

--- src/app/quadro/cores/cores.component.ts
+++ src/app/quadro/cores/cores.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit, Output, EventEmitter } from '@angular/core';
 import { Cor } from './cor.model';
+import { CoresService } from '../../shared';
 
 @Component({
   selector: 'mk-cores',
@@ -10,19 +11,23 @@ export class CoresComponent implements OnInit {
 
   @Output() alteradoCor = new EventEmitter<string>();
 
-  cores: Array<Cor> = [
-    new Cor({hex: '#1c1b20'}),
-    new Cor({hex: '#f4f6fb'})
-  ];
+  cores: Array<Cor> = [];
 
-  constructor() { }
+  constructor(private corService: CoresService) { }
 
   ngOnInit() {
+    this.obterTodasCores();
   }
 
   alterarCor(cor: string) {
     console.log('Solicitando alteração de cor da borda.', cor);
     this.alteradoCor.emit(cor);
+  }
+
+  obterTodasCores() {
+    this.corService.getAll().subscribe(
+      (data: any) => { this.cores = data; },
+      error => console.log(error));
   }
 
 }
--- src/app/shared/index.ts
+++ src/app/shared/index.ts
@@ -1,1 +1,2 @@
 export * from './quadros.service';
+export * from './cores.service';
